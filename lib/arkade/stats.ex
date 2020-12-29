c34d8faf39aaba04d374ab8327862ab0ec58ad29
julienurraca@gmail.com <julienurraca@gmail.com>
2020-12-29 16:52:13 -0500

    add share_price query

--- lib/arkade/stats.ex
+++ lib/arkade/stats.ex
@@ -3,10 +3,27 @@ defmodule Arkade.Stats do
   import Ecto.Query
 
   def by_fund() do
-    Arkade.Holding
+    Holding
     |> group_by([h], [h.date, h.fund])
     |> select([h], [h.date, h.fund, sum(h.market_value)])
     |> Repo.all()
     |> Enum.map(fn [date, fund, value] -> %{date: date, fund: fund, market_value: value} end)
+  end
+
+  def get_share_price(ticker, since \\ ~D[2020-12-21]) do
+    Holding
+    |> where([h], h.ticker == ^ticker)
+    |> where([h], h.date > ^since)
+    |> select([h], [h.date, h.shares, h.market_value])
+    |> Repo.all()
+    |> Enum.map(fn row -> calc_unit_price(row, ticker) end)
+  end
+
+  def calc_unit_price([date, shares, market_value], ticker) do
+    %{
+      ticker: ticker,
+      date: date,
+      share_price: Float.round(market_value / shares, 2)
+    }
   end
 end
