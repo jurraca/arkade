d2e9d013c625ac5227256c785cf7ed1902031382
julienurraca@gmail.com <julienurraca@gmail.com>
2021-01-20 14:57:55 -0500

    fix parser

--- lib/arkade/csv_parser.ex
+++ lib/arkade/csv_parser.ex
@@ -1,6 +1,6 @@
 defmodule Arkade.Csv do
   @moduledoc """
-      Take in a CSV, format fields, return a data structure that matches the DB schema. 
+      Take in a CSV, format fields, return a data structure that matches the DB schema.
   """
 
   NimbleCSV.define(Parser, [])
@@ -11,11 +11,10 @@ defmodule Arkade.Csv do
     |> Enum.map(fn row -> format_row(row) end)
   end
 
-  @doc """
-    Take in a parsed row, and attempt to extract data. 
-    There are empty rows with 8 cells after the data, which we throw out. 
-    There is a message and 7 cells after those empty rows, which we also throw out. 
-  """
+# Take in a parsed row, and attempt to extract data.
+# There are empty rows with 8 cells after the data, which we throw out.
+# There is a message and 7 cells after those empty rows, which we also throw out.
+
   defp format_row([_head, "", "", "", "", "", ""]) do
     nil
   end
@@ -45,10 +44,30 @@ defmodule Arkade.Csv do
   end
 
   defp format_date([month, day, year]) do
-    Enum.join([year, month, day], "-")
+    [m, d, y] = [month, day, year] |> pad_zeros()
+
+    Enum.join([y, m, d],  "-")
   end
 
   defp format_date(_), do: "1970-01-01"
+
+  def pad_zeros(list) do
+    pad_zeros(list, [])
+  end
+
+  def pad_zeros([head | tail], state) do
+    fixed = pad_zero(head)
+    pad_zeros(tail, state ++ [ fixed ] )
+  end
+
+  def pad_zeros([], state), do: state
+
+  def pad_zero(string) do
+    case Kernel.byte_size(string) do
+      1 -> "0" <> string
+      _ -> string
+    end
+  end
 
   defp round_float(float) do
     case Integer.parse(float) do
